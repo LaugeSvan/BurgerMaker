fc4dc89dbfefedb712dac458169eefab3e5717d9
LaugeSvan <laugessvan@gmail.com>
2025-07-30 10:28:59 +0200

    Add a new feature to load burgers by ID and log ID to console

---
+++ script.js
@@ -0,0 +1,5 @@
+// Define functions and variables at the top
+function getBurgerId() {
+    var loadBurgerId = prompt("Please enter the ID of the burger you want to load:");
+    console.log("ID to load:", loadBurgerId);
+}
